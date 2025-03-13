52987fad42f27ad50d797f622a717148c33f0f09
jerichomc <jerichomc255@gmail.com>
2025-03-12 21:11:09 -0700

    fixed resume not downloading

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,7 +5,7 @@ function Resume() {
         <div className="resume-container">
             <h1>Resume</h1>
             <p>Click the button below to download my resume.</p>
-            <a href="/resume.docx" download="Jericho_McCullough_Resume.docx">
+            <a href={`${process.env.PUBLIC_URL}/resume.docx`} download="Jericho_McCullough_Resume.docx">
                 <button className="download-btn">Download Resume</button>
             </a>
         </div>
